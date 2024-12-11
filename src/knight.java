6da8a8ecf324adea1a79ac3b8ad75dc5ca61123c
19ong_y <19ong_y@bexleygs.co.uk>
2024-12-11 14:15:24 +0000

    dfgafsd

--- src/knight.java
+++ src/knight.java
@@ -8,7 +8,7 @@ public class knight extends Piece{
         int rowDiff = positionE[0] - positionS[0];
         int colDiff = positionE[1] - positionS[1];
 
-        return (rowDiff == 2 || rowDiff == -2) && (colDiff == 1 || colDiff == -1);
+        return ((rowDiff == 2 || rowDiff == -2) && (colDiff == 1 || colDiff == -1)) || (Math.abs(rowDiff) == 1) && (Math.abs(colDiff) == 2);
         //if it move like a L shape lying down
 
     }
